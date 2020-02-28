b253bc851dd6984cd9f2519cb535aa9c6777604e
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-28 05:40:06 +0530

    define fields of CoffeeMachine.java

--- src/coffeeMachine/CoffeeMachine.java
+++ src/coffeeMachine/CoffeeMachine.java
@@ -4,4 +4,9 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     private Scanner scanner;
+    private int water;
+    private int milk;
+    private int beans;
+    private int cups;
+    private int money;
 }
