68acc56b8c6996941db9ec9fd37dab5a7ecbcd38
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-28 05:41:15 +0530

    create constructor of CoffeeMachine.java for intialisation of fields with intial values

--- src/coffeeMachine/CoffeeMachine.java
+++ src/coffeeMachine/CoffeeMachine.java
@@ -9,4 +9,13 @@ public class CoffeeMachine {
     private int beans;
     private int cups;
     private int money;
+    private CoffeeMachine() {
+        this.scanner = new Scanner(System.in);
+        this.water = 400;
+        this.milk = 540;
+        this.beans = 120;
+        this.cups = 9;
+        this.money = 550;
+    }
+
 }
